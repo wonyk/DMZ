e3913394852b7f20e82e0f97db487704573b8ea9
wonyk <yingkeatwon@gmail.com>
2019-01-20 10:39:59 +0800

    Working game

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,10 @@
+/* eslint-disable standard/array-bracket-even-spacing */
+/* eslint-disable no-sparse-arrays */
 import '../../node_modules/kontra/kontra'
-import test1 from '../img/test1.jpg'
-import test2 from '../img/test2.jpg'
 import { starting, common, rare } from '../data/data.json'
+import TinyMusic from './tinyMusic'
+const gameplayCards = require.context('../img', true)
+const imagePath = (name) => gameplayCards(name, true)
 const letters = {
   'A': [
     [, 1],
@@ -291,9 +294,16 @@ const letters = {
     [0, 0, 0]
   ]
 }
+/*
+==============================================================================
+INIT GAME
+=============================================================================
+*/
+
 kontra.init('game')
 const canvas = kontra.canvas
 const context = kontra.context
+// Thanks to Pixel Font for this library. Link: https://github.com/PaulBGD/PixelFont
 function draw (posX, posY, string, size) {
   context.clearRect(posX, posY, canvas.width, canvas.height)
   var needed = []
@@ -336,6 +346,155 @@ File and image imports
 //     console.log(err)
 //   })
 
+// Audio Loading
+// create the audio context
+// Thanks to TinyMusic.js for this wonderful library and sample music =)
+let ac = new AudioContext()
+// get the current Web Audio timestamp (this is when playback should begin)
+
+let when = ac.currentTime
+
+// set the tempo
+let tempo = 132
+// initialize some lets
+let sequence1
+let sequence2
+let sequence3
+
+// create an array of "note strings" that can be passed to a sequence
+
+let lead = [
+  '-   e',
+  'Bb3 e',
+  'A3  e',
+  'Bb3 e',
+  'G3  e',
+  'A3  e',
+  'F3  e',
+  'G3  e',
+
+  'E3  e',
+  'F3  e',
+  'G3  e',
+  'F3  e',
+  'E3  e',
+  'F3  e',
+  'D3  q',
+
+  '-   e',
+  'Bb3 s',
+  'A3  s',
+  'Bb3 e',
+  'G3  e',
+  'A3  e',
+  'G3  e',
+  'F3  e',
+  'G3  e',
+
+  'E3  e',
+  'F3  e',
+  'G3  e',
+  'F3  e',
+  'E3  s',
+  'F3  s',
+  'E3  e',
+  'D3  q'
+]
+
+let harmony = [
+  '-   e',
+  'D4  e',
+  'C4  e',
+  'D4  e',
+  'Bb3 e',
+  'C4  e',
+  'A3  e',
+  'Bb3 e',
+
+  'G3  e',
+  'A3  e',
+  'Bb3 e',
+  'A3  e',
+  'G3  e',
+  'A3  e',
+  'F3  q',
+
+  '-   e',
+  'D4  s',
+  'C4  s',
+  'D4  e',
+  'Bb3 e',
+  'C4  e',
+  'Bb3 e',
+  'A3  e',
+  'Bb3 e',
+
+  'G3  e',
+  'A3  e',
+  'Bb3 e',
+  'A3  e',
+  'G3  s',
+  'A3  s',
+  'G3  e',
+  'F3  q'
+]
+
+let bass = [
+  'D3  q',
+  '-   h',
+  'D3  q',
+
+  'A2  q',
+  '-   h',
+  'A2  q',
+
+  'Bb2 q',
+  '-   h',
+  'Bb2 q',
+
+  'F2  h',
+  'A2  h'
+]
+
+// create 3 new sequences (one for lead, one for harmony, one for bass)
+sequence1 = new TinyMusic.Sequence(ac, tempo, lead)
+sequence2 = new TinyMusic.Sequence(ac, tempo, harmony)
+sequence3 = new TinyMusic.Sequence(ac, tempo, bass)
+
+// set staccato and smoothing values for maximum coolness
+sequence1.staccato = 0
+sequence1.smoothing = 0.5
+sequence2.staccato = 0.55
+sequence3.staccato = 0.05
+sequence3.smoothing = 0.4
+
+// adjust the levels so the bass and harmony aren't too loud
+sequence1.gain.gain.value = 1.0
+sequence2.gain.gain.value = 0.8
+sequence3.gain.gain.value = 0.65
+
+// apply EQ settings
+sequence1.mid.frequency.value = 800
+sequence1.mid.gain.value = 3
+sequence2.mid.frequency.value = 1200
+sequence3.mid.gain.value = 3
+sequence3.bass.gain.value = 6
+sequence3.bass.frequency.value = 80
+sequence3.mid.gain.value = -6
+sequence3.mid.frequency.value = 500
+sequence3.treble.gain.value = -2
+sequence3.treble.frequency.value = 1400
+
+// Apply waveType
+sequence1.waveType = 'sine'
+
+// start the lead part immediately
+sequence1.play(when)
+// delay the harmony by 16 beats
+sequence2.play(when + (60 / tempo) * 16)
+// start the bass part immediately
+sequence3.play(when)
+
 /*
 =======================================================================================
 KONTRA browser constants and defaults
@@ -343,9 +502,93 @@ KONTRA browser constants and defaults
 */
 let score = 0
 let mainPicIndex = 0
+// Creates shallow copy to prevent issues
+let arrayCommon = common.slice(0)
+let rareAppeared = false
+let isRare = false
+let gameEnded = false
 let randomCardObject = {}
+// Start with the rules card as a standard
+randomCardObject = starting
 
-randomCardObject = common[0]
+/*
+=====================================================================================
+MAIN GAME FUNCTION
+=====================================================================================
+*/
+
+// Change Card and Update Score
+const changeCard = (selection) => {
+  const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max))
+  console.log(arrayCommon.length)
+  if (arrayCommon.length === 1) {
+    arrayCommon = common.slice(0)
+  }
+  // For starting rule cards
+  if (randomCardObject.title === 'Rules and Start Card') {
+    mainPicIndex = getRandomInt(arrayCommon.length)
+    randomCardObject = arrayCommon[mainPicIndex]
+    image.src = imagePath(randomCardObject.src)
+    return
+  }
+  // If it is one of the ending cards, do nothing
+  if (!randomCardObject.title || gameEnded) {
+    return
+  }
+
+  // Edit the Stats Bar then checked for issues
+  serverBar.width += (randomCardObject[selection].server * 2)
+  moneyBar.width += (randomCardObject[selection].money * 2)
+  trustBar.width += (randomCardObject[selection].trust * 2)
+
+  // Process higher limit
+  if (serverBar.width > 200) {
+    serverBar.width = 200
+  }
+  if (moneyBar.width > 200) {
+    moneyBar.width = 200
+  }
+  if (trustBar.width > 200) {
+    trustBar.width = 200
+  }
+  // Process lower limit
+  if (trustBar.width <= 0) {
+    trustBar.width = 0
+    image.src = imagePath('./endTrust.jpg')
+    gameEnded = true
+  }
+  if (serverBar.width <= 0) {
+    serverBar.width = 0
+    image.src = imagePath('./endServer.jpg')
+    gameEnded = true
+  }
+  if (moneyBar.width <= 0) {
+    moneyBar.width = 0
+    image.src = imagePath('./endMoney.jpg')
+    gameEnded = true
+    // This part below comes only if the player have low health but not dead
+  } else if (isRare) {
+    mainPicIndex = getRandomInt(arrayCommon.length)
+    score += randomCardObject.days
+    randomCardObject = arrayCommon[mainPicIndex]
+    image.src = imagePath(randomCardObject.src)
+    isRare = false
+  } else if ((trustBar.width <= 20 || moneyBar.width <= 20 || serverBar.width <= 20) && !rareAppeared) {
+    // Some help so that players dont die too quickly
+    arrayCommon.splice(mainPicIndex, 1)
+    score += randomCardObject.days
+    randomCardObject = rare
+    image.src = imagePath(randomCardObject.src)
+    rareAppeared = true
+    isRare = true
+  } else {
+    arrayCommon.splice(mainPicIndex, 1)
+    score += randomCardObject.days
+    mainPicIndex = getRandomInt(arrayCommon.length)
+    randomCardObject = arrayCommon[mainPicIndex]
+    image.src = imagePath(randomCardObject.src)
+  }
+}
 
 /*
 ========================================================================================
@@ -354,13 +597,8 @@ SPRITES and MISC
 */
 
 let highscore = kontra.sprite({
-  // update: function () {
-  //   let a = kontra.store.get('highscore')
-  //   kontra.store.set('highscore', a++)
-  // },
   render: function () {
     // draw (x, y, text, size)
-    // draw(15, 15, `Days:${kontra.store.get('highscore')}`, 10)
     draw(15, 15, `Days:${Math.round(score)}`, 10)
   }
 })
@@ -395,7 +633,6 @@ let serverBar = kontra.sprite({
     this.context.strokeRect(this.x, this.y, 200, this.height)
   }
 })
-
 
 let moneyBar = kontra.sprite({
   x: 1025,
@@ -441,13 +678,16 @@ let dealButton = kontra.sprite({
   height: 100
 })
 
-let image = new Image()
-image.src = test1
+let helpWords = kontra.sprite({
+  render: function () {
+    draw(900, 630, 'MEH!', 5)
+    draw(1120, 630, 'DEAL', 5)
+  }
+})
 
-// setTimeout(function () {
-//   image.src = test2
-//   kontra.store.set('highscore', 1)
-// }, 10000)
+let image = new Image()
+image.src = imagePath(randomCardObject.src)
+
 let mainImage = kontra.sprite({
   x: 330,
   y: 120,
@@ -463,8 +703,6 @@ GAMELOOP
 */
 // let startLoop = function () {
 let loop = kontra.gameLoop({ // create the main game loop
-  // clearCanvas: false,
-  fps: 20,
   update () { // update the game state
     // highscore.update()
     // serverBar.update()
@@ -479,6 +717,7 @@ let loop = kontra.gameLoop({ // create the main game loop
     trustBar.render()
     hellNoButton.render()
     dealButton.render()
+    helpWords.render()
   }
 })
 // }
@@ -488,14 +727,31 @@ let loop = kontra.gameLoop({ // create the main game loop
   KEYBOARD
   =======================================================================================
   */
-// kontra.keys.bind('p', () => {
-//   loop.stop()
-// })
 
 kontra.keys.bind('left', (e) => {
   e.preventDefault()
-  kontra.store.set('highscore', 2)
-  image.src = test2
+  changeCard('meh')
+})
+
+kontra.keys.bind('right', (e) => {
+  e.preventDefault()
+  changeCard('deal')
+})
+
+kontra.keys.bind('enter', (e) => {
+  e.preventDefault()
+  if (gameEnded) {
+    score = 0
+    mainPicIndex = 0
+    arrayCommon = common
+    rareAppeared = false
+    gameEnded = false
+    randomCardObject = starting
+    image.src = imagePath(randomCardObject.src)
+    serverBar.width = 100
+    moneyBar.width = 100
+    trustBar.width = 100
+  }
 })
 
 /*
@@ -507,12 +763,10 @@ kontra.pointer.track(hellNoButton)
 kontra.pointer.track(dealButton)
 kontra.pointer.onDown((event, object) => {
   if (object === hellNoButton) {
-    serverBar.width += (randomCardObject.meh.server)
-    score += randomCardObject.days
+    changeCard('meh')
   }
   if (object === dealButton) {
-    kontra.store.set('highscore', 5)
-    image.src = test1
+    changeCard('deal')
   }
 })
 
